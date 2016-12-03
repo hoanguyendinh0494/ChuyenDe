5d887166414409464a284a819ab4d4e96596cdb7
Hoà Nguyễn <lucas@lucass-macbook-pro.local>
2016-12-03 15:42:03 +0700

    Initial Commit

---
+++ English4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  English4
+//
+//  Created by Lucas on 12/3/16.
+//  Copyright © 2016 Lucas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
